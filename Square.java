b8c54970ffa45f28bea387d94ec754e82d06a3cf
4778477 <50234533+4778477@users.noreply.github.com>
2019-05-15 12:13:33 +0100

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Square {
 
 	private Dictionary dictionary;
+	private ArrayList<String> usedChars = new ArrayList<>();
 
 	Square(Dictionary dictionary) {
 		this.dictionary = dictionary;
@@ -10,14 +12,76 @@ public class Square {
 
 	void build(int numChars, String alphaChars) {
 		ArrayList<String> remainingChars = new ArrayList<>();
+		ArrayList<String> firstWords = new ArrayList<>();
+		ArrayList<String> secondWords = new ArrayList<>();
+		ArrayList<String> thirdWords = new ArrayList<>();
+		ArrayList<String> fourthWords = new ArrayList<>();
 		ArrayList<String> currentSquare = new ArrayList<>();
-		for (char letter : alphaChars.toCharArray()){
+		currentSquare.add("");
+		currentSquare.add("");
+		currentSquare.add("");
+		currentSquare.add("");
+
+		for (char letter : alphaChars.toCharArray()) {
 			remainingChars.add(String.valueOf(letter));
 		}
-		System.out.println(remainingChars);
-		currentSquare.add(dictionary.getDictionary().get(0));
-		System.out.println(currentSquare);
+		Collections.sort(remainingChars, String.CASE_INSENSITIVE_ORDER);
 
+		firstWords.addAll(dictionary.getDictionary());
+
+		for (String word : firstWords){
+			usedChars.clear();
+			currentSquare.set(0, word);
+			updateUsedCharacters(currentSquare);
+			String firstPrefix = String.valueOf(currentSquare.get(0).charAt(1));
+			secondWords.clear();
+			secondWords.addAll(dictionary.getWordsByPrefix(firstPrefix));
+
+			for (String word2 : secondWords){
+				usedChars.clear();
+				currentSquare.set(1, word2);
+				updateUsedCharacters(currentSquare);
+				String secondPrefix = String.valueOf(currentSquare.get(0).charAt(2) + "" + currentSquare.get(1).charAt(2));
+				thirdWords.clear();
+				thirdWords.addAll(dictionary.getWordsByPrefix(secondPrefix));
+
+				for (String word3 : thirdWords){
+					usedChars.clear();
+					currentSquare.set(2, word3);
+					updateUsedCharacters(currentSquare);
+					String thirdPrefix = String.valueOf(currentSquare.get(0).charAt(3) + "" + currentSquare.get(1).charAt(3) + "" + currentSquare.get(2).charAt(3));
+					fourthWords.clear();
+					fourthWords.addAll(dictionary.getWordsByPrefix(thirdPrefix));
+
+					for (String word4 : fourthWords){
+						usedChars.clear();
+						currentSquare.set(3, word4);
+						updateUsedCharacters(currentSquare);
+						if (usedChars.equals(remainingChars)){
+							drawSquare(currentSquare);
+						}
+					}
+
+				}
+
+			}
+
+		}
 	}
 
+	private void updateUsedCharacters(ArrayList<String> currentSquare){
+		for (String squareWord : currentSquare){
+			for (char ch : squareWord.toCharArray()){
+				usedChars.add(String.valueOf(ch));
+			}
+		}
+		Collections.sort(usedChars, String.CASE_INSENSITIVE_ORDER);
+	}
+
+	private void drawSquare(ArrayList<String> currentSquare){
+		System.out.println();
+		for (String word : currentSquare){
+			System.out.println(word);
+		}
+	}
 }
